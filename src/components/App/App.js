a1ac07a48e8a8ac0475f140b3e48bdf3b653c8ea
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-01-05 19:40:21 +0000

    Remove hard coded information, integrate app with api.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,32 +3,22 @@ import "./App.css";
 
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
-
-const business = {
-  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Flavortown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
-
-const businessesArray = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business,
-];
+import Yelp from "../../util/Yelp";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: [],
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log(
-      `Searching Yelp with term: ${term}, location: ${location}, sortBy: ${sortBy}`
+    console.log(term, location, sortBy);
+    Yelp.search(term, location, sortBy).then((businesses) =>
+      this.setState({ businesses: businesses })
     );
   }
 
@@ -37,7 +27,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businessesArray} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -79,9 +79,7 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
-          <a href="http://localhost:3000/#" onClick={this.handleSearch}>
-            Let's Go
-          </a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
